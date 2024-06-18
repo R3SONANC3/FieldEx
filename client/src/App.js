44c4ea3680e2973a35267e3790989882899f9c5e
R3SONANC3 <jeerapat5870@gmail.com>
2024-06-18 22:49:44 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Home from "./components/Home";
 import SelectForm from "./components/SelectForm";
@@ -16,28 +16,47 @@ import LocalOperaSecond from "./components/form/Local/LocalOperaSecond";
 import LocalOperaThird from "./components/form/Local/LocalOperaThird";
 import LocalResult from "./components/form/Local/LocalResult";
 import ForgorPassword from "./admin/ForgorPassword";
+import Loding from './components/Loading'
+
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a 2-second loading time
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admindashboard" element={<AdminDashboard/>}/>
-        <Route path="/selectform" element={<SelectForm />} />
-        <Route path="/evaluation" element={<EvaluationPage />} />
-        <Route path="/generalform" element={<GeneralForm />} />
-        <Route path="/formmanagement" element={<FormManagement />} />
-        <Route path="/about" element={<About /> } />
-        <Route path="/localform" element={<LocalGovernmentForm  />} />
-        <Route path="/localconscript" element={<Conscript />} />
-        <Route path="/localsummary" element={<Summary />} />
-        <Route path="/localmanage" element={<LocalManage />} />
-        <Route path="/localoperafirst" element={<LocalOperaFirst />} />
-        <Route path="/localoperasec" element={<LocalOperaSecond />}/>
-        <Route path="/localoperathird" element={<LocalOperaThird />}/>
-        <Route path="/localresult" element={<LocalResult />}/>
-        <Route path="/forgotpass" element={<ForgorPassword />}/>      
-      </Routes>
+      {isLoading ? (
+        <div style={{ textAlign: "center"}}>
+          <Loding />
+        </div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admindashboard" element={<AdminDashboard />} />
+          <Route path="/selectform" element={<SelectForm />} />
+          <Route path="/evaluation" element={<EvaluationPage />} />
+          <Route path="/generalform" element={<GeneralForm />} />
+          <Route path="/formmanagement" element={<FormManagement />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/localform" element={<LocalGovernmentForm />} />
+          <Route path="/localconscript" element={<Conscript />} />
+          <Route path="/localsummary" element={<Summary />} />
+          <Route path="/localmanage" element={<LocalManage />} />
+          <Route path="/localoperafirst" element={<LocalOperaFirst />} />
+          <Route path="/localoperasec" element={<LocalOperaSecond />} />
+          <Route path="/localoperathird" element={<LocalOperaThird />} />
+          <Route path="/localresult" element={<LocalResult />} />
+          <Route path="/forgotpass" element={<ForgorPassword />} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
